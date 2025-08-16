f2129a43e77f61c586cef3b4766883a6fd0dbe69
mirza36 <mirzamohd1214@gmail.com>
2025-08-16 05:08:36 +0300

    first commit

---
+++ src/api/insta/routes/insta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * insta router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::insta.insta');
---
+++ src/api/insta/services/insta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * insta service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::insta.insta');
