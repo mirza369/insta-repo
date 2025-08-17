c4465f981cb94de5ce9bc0022b068c2bc77b5f5a
mirza36 <mirzamohd1214@gmail.com>
2025-08-17 16:44:31 +0300

    added usernme field

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -432,6 +432,7 @@ export interface ApiInstaInsta extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    username: Schema.Attribute.String;
   };
 }
 
